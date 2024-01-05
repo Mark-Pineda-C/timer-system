ffa5a8b7258aac18832542f1fa47adb71c99d659
Mark Pineda <mfpc1500@gmail.com>
2024-01-05 08:20:16 -0500

    translations

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,20 +3,22 @@
 import { Input, Button } from "@nextui-org/react"
 import { MaterialSymbolsPersonRounded, MaterialSymbolsLockOpenRight, MaterialSymbolsLock } from "./svg"
 import { useState } from "react";
+import { useTranslations } from "next-intl";
 
 export const LoginForm = () => {
   const [visible, setVisible] = useState(false);
+  const t = useTranslations('Login');
   return (
     <div className="py-5 flex flex-col w-full">
-      <Input label="Usuario" className="w-full" variant="underlined" color="primary" endContent={
+      <Input label={t('user')} className="w-full" variant="underlined" color="primary" endContent={
         <MaterialSymbolsPersonRounded className="text-xl text-primary-500"/>
       }/>
-      <Input className="w-full" label="Contraseña" variant="underlined" color="primary" type={visible ? "default" : "password"} endContent={
+      <Input className="w-full" label={t('password')} variant="underlined" color="primary" type={visible ? "default" : "password"} endContent={
         <button onClick={() => setVisible(!visible)}>
           {visible ? <MaterialSymbolsLockOpenRight className="text-lg text-primary-500"/> : <MaterialSymbolsLock className="text-lg text-primary-500"/>}
         </button>
       }/>
-      <Button className="mt-10" color="primary">Iniciar sesión</Button>
+      <Button className="mt-10" color="primary">{t('title')}</Button>
     </div>
   )
 }
